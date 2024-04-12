bef8751cfb7c067940b67d035c1c11bd0f11ff33
Rafael Souza <rpsouza.dev@gmail.com>
2024-04-12 16:06:08 -0300

    Add register fragment styles

---
+++ app/src/main/java/com/rpsouza/bancodigital/presenter/auth/register/RegisterFragment.kt
@@ -0,0 +1,26 @@
+package com.rpsouza.bancodigital.presenter.auth.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rpsouza.bancodigital.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+  private var _binding: FragmentRegisterBinding? = null
+  private val binding get() = _binding!!
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    _binding = null
+  }
+}
