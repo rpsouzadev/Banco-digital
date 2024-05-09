5b6d92946052a483d9efb68405a34e24f41f5965
Rafael Souza <rpsouza.dev@gmail.com>
2024-05-09 20:17:51 -0300

    Add TransferUserListViewModel

---
+++ app/src/main/java/com/rpsouza/bancodigital/domain/transaction/TransferUserListViewModel.kt
@@ -0,0 +1,27 @@
+package com.rpsouza.bancodigital.domain.transaction
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.rpsouza.bancodigital.domain.profile.GetProfileListUseCase
+import com.rpsouza.bancodigital.utils.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+@HiltViewModel
+class TransferUserListViewModel @Inject constructor(
+  private val getProfileListUseCase: GetProfileListUseCase
+) : ViewModel() {
+
+  fun getProfileList() = liveData(Dispatchers.IO) {
+    try {
+      emit(StateView.Loading())
+
+      val profileList = getProfileListUseCase.invoke()
+
+      emit(StateView.Success(profileList))
+    } catch (ex: Exception) {
+      emit(StateView.Error(ex.message))
+    }
+  }
+}
